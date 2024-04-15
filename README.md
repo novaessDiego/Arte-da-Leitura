f5783e626e39ab2c20ea0b4ac62d3b868143438d
Diego <novaessdiego@gmail.com>
2024-04-15 15:05:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Arte da Leitura
+## Sistema de Gerenciamento para Bibliotecas Escolares
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>Arte da Leitura</h1>
+    </>
+  )
+}
+
+export default App
